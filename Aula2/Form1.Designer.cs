d42ba7078f93175c87c65915fe5a2be3b978c7a9
JESSICA BATISTA DOS SANTOS <jessica.bsantos22@senacsp.edu.br>
2025-04-22 22:02:22 -0300

    Adicionar arquivos de projeto.

---
+++ Aula2/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace Aula2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNome = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.txtTelefone = new System.Windows.Forms.TextBox();
+            this.lblTelefone = new System.Windows.Forms.Label();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtBuscar = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lstContatos = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(26, 19);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(35, 13);
+            this.lblNome.TabIndex = 0;
+            this.lblNome.Text = "Nome";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(29, 36);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(100, 20);
+            this.txtNome.TabIndex = 1;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(29, 90);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(100, 20);
+            this.txtEmail.TabIndex = 3;
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(26, 73);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(35, 13);
+            this.lblEmail.TabIndex = 2;
+            this.lblEmail.Text = "E-mail";
+            // 
+            // txtTelefone
+            // 
+            this.txtTelefone.Location = new System.Drawing.Point(29, 150);
+            this.txtTelefone.Name = "txtTelefone";
+            this.txtTelefone.Size = new System.Drawing.Size(100, 20);
+            this.txtTelefone.TabIndex = 5;
+            // 
+            // lblTelefone
+            // 
+            this.lblTelefone.AutoSize = true;
+            this.lblTelefone.Location = new System.Drawing.Point(26, 133);
+            this.lblTelefone.Name = "lblTelefone";
+            this.lblTelefone.Size = new System.Drawing.Size(49, 13);
+            this.lblTelefone.TabIndex = 4;
+            this.lblTelefone.Text = "Telefone";
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Location = new System.Drawing.Point(39, 188);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(75, 23);
+            this.btnSalvar.TabIndex = 6;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(697, 34);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Buscar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // txtBuscar
+            // 
+            this.txtBuscar.Location = new System.Drawing.Point(153, 36);
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(543, 20);
+            this.txtBuscar.TabIndex = 13;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(150, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Buscar Contato:";
+            // 
+            // lstContatos
+            // 
+            this.lstContatos.HideSelection = false;
+            this.lstContatos.Location = new System.Drawing.Point(153, 60);
+            this.lstContatos.Name = "lstContatos";
+            this.lstContatos.Size = new System.Drawing.Size(619, 143);
+            this.lstContatos.TabIndex = 11;
+            this.lstContatos.UseCompatibleStateImageBehavior = false;
+            this.lstContatos.ItemSelectionChanged += new System.Windows.Forms.ListViewItemSelectionChangedEventHandler(this.lstContatos_ItemSelectionChanged);
+            this.lstContatos.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(809, 232);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtBuscar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstContatos);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.txtTelefone);
+            this.Controls.Add(this.lblTelefone);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.lblNome);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.TextBox txtTelefone;
+        private System.Windows.Forms.Label lblTelefone;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtBuscar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView lstContatos;
+    }
+}
+
---
+++ Aula2/Form1.cs
@@ -0,0 +1,336 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Aula2
+{
+    public partial class Form1: Form
+    {
+        MySqlConnection Conexao;
+
+        // padrão: host user senha
+        private string data_source = "datasource=localhost;username=root;password=;database=aulas_uc3";
+
+        public int? id_contato_selecionado = null;
+        public Form1()
+        {
+            InitializeComponent();
+
+
+
+            // configurações
+            lstContatos.View = View.Details;
+            lstContatos.LabelEdit = true;
+            lstContatos.AllowColumnReorder = true;
+            lstContatos.FullRowSelect = true;
+            lstContatos.GridLines = true;
+
+
+            // Posição dos Cabeçalhos a serem exibidos na tela
+            lstContatos.Columns.Add("ID", 30, HorizontalAlignment.Left);
+            lstContatos.Columns.Add("Nome", 150, HorizontalAlignment.Left);
+            lstContatos.Columns.Add("Email", 150, HorizontalAlignment.Left);
+            lstContatos.Columns.Add("Telefone", 150, HorizontalAlignment.Left);
+
+            carregar_contatos();
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                // Criar a conexão com o MySQL
+                Conexao = new MySqlConnection(data_source);
+                Conexao.Open();
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.Connection = Conexao;
+
+                if (id_contato_selecionado == null)
+                {
+                    // insert
+                    cmd.Parameters.Clear(); // limpa os parâmetros antigos
+                    cmd.CommandText =
+                        "INSERT INTO contato " +
+                        "(nome, email, telefone) " +
+                        "VALUES " +
+                        "(@nome, @email, @telefone)";
+
+                    cmd.Parameters.AddWithValue("@nome", txtNome.Text);
+                    cmd.Parameters.AddWithValue("@email", txtEmail.Text);
+                    cmd.Parameters.AddWithValue("@telefone", txtTelefone.Text);
+
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Contato Inserido com Sucesso", "Sucesso",
+                                    MessageBoxButtons.OK,
+                                    MessageBoxIcon.Information);
+                }
+                else
+                {
+                    // update
+                    cmd.Parameters.Clear(); // limpa os parâmetros antigos
+                    cmd.CommandText =
+                        "UPDATE contato " +
+                        "SET nome = @nome, email = @email, telefone = @telefone " +
+                        "WHERE id = @id";
+
+                    cmd.Parameters.AddWithValue("@nome", txtNome.Text);
+                    cmd.Parameters.AddWithValue("@email", txtEmail.Text);
+                    cmd.Parameters.AddWithValue("@telefone", txtTelefone.Text);
+                    cmd.Parameters.AddWithValue("@id", id_contato_selecionado);
+
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Contato Atualizado com Sucesso", "Sucesso",
+                                    MessageBoxButtons.OK,
+                                    MessageBoxIcon.Information);
+                }
+                /* string sql = "INSERT INTO contato (nome, email, telefone) VALUES ('" + txtNome.Text + "','" + txtEmail.Text + "','" + txtTelefone.Text + "')";
+
+               // Executar Comando Insert
+               MySqlCommand insert = new MySqlCommand(sql, Conexao);
+
+               Conexao.Open();
+               insert.ExecuteReader(); */
+
+
+                // Mostrando a Mensagem para o Usuário
+                //MessageBox.Show("Dados Inseridos com Sucesso!!!");
+                // apagando o formulario após a inserção dos dados
+                id_contato_selecionado = null;
+                txtNome.Text = String.Empty;
+                txtEmail.Text = String.Empty;
+                txtTelefone.Text = String.Empty;
+
+                carregar_contatos();
+
+                #region Código Antigo
+                // SQL que desejamos executar - marcadores
+                //cmd.CommandText =
+                //    "INSERT INTO contato " +
+                //    "(nome, email, telefone) " +
+                //    "VALUES" +
+                //    "(@nome, @email, @telefone)";
+
+                //cmd.Parameters.AddWithValue("@nome", txtNome.Text);
+                //cmd.Parameters.AddWithValue("@email", txtEmail.Text);
+                //cmd.Parameters.AddWithValue("@telefone", txtTelefone.Text);
+
+                //cmd.ExecuteNonQuery();
+
+                /* string sql = "INSERT INTO contato (nome, email, telefone) VALUES ('" + txtNome.Text + "','" + txtEmail.Text + "','" + txtTelefone.Text + "')";
+
+                // Executar Comando Insert
+                MySqlCommand insert = new MySqlCommand(sql, Conexao);
+
+                Conexao.Open();
+                insert.ExecuteReader(); */
+
+
+                // Mostrando a Mensagem para o Usuário
+                //MessageBox.Show("Dados Inseridos com Sucesso!!!");
+                //MessageBox.Show("Contato Inserido com Sucesso ", "Sucesso ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                #endregion
+
+
+            }
+            catch (MySqlException ex)
+
+            {
+                MessageBox.Show("Error " + "has occured: " + ex.Message,
+                    "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Has occured: " + ex.Message,
+                    "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                Conexao.Close();
+            }
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void carregar_contatos()
+        {
+            try
+            {
+
+                // Criar a conexão com o MySQL
+                Conexao = new MySqlConnection(data_source);
+
+                string sql = "SELECT * FROM contato ORDER BY id ASC";
+
+                Conexao.Open();
+
+                // Buscar as informações
+                MySqlCommand buscar = new MySqlCommand(sql, Conexao);
+
+                // armazena as informacoes que temos na busca para mostrar na tela
+                MySqlDataReader reader = buscar.ExecuteReader();
+
+                // como iremos mostrar os dados na tela para o usuário
+                lstContatos.Items.Clear();
+
+                while (reader.Read())
+                {
+                    string[] row =
+                    {
+                        // obtendo as informações do banco de dados (vetor de strings)
+                        reader.GetInt32(0).ToString(), // id
+                        reader.GetString(1),           // nome
+                        reader.GetString(2),           // email
+                        reader.GetString(3),           // telefone
+                    };
+
+                    var linha_list_view = new ListViewItem(row);
+                    lstContatos.Items.Add(linha_list_view);
+                }
+
+
+            }
+            catch (MySqlException ex)
+
+            {
+                MessageBox.Show("Error " + "has occured: " + ex.Message,
+                    "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Has occured: " + ex.Message,
+                    "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                Conexao.Close();
+            }
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ListView.SelectedListViewItemCollection itens_selecionados = lstContatos.SelectedItems;
+
+            /* percorrendo a coleção de itens dentro da lista itens_selecionados
+            // Obs¹: A minha linha toda é um item, que contem os subItems (colunas) que desejo selecionar
+
+            foreach (ListViewItem item in itens_selecionados)
+            {
+                id_contato_selecionado = Convert.ToInt32(item.SubItems[0].Text);
+                // extrai o valor de cada uma das variáveis (colunas)
+                txtNome.Text = item.SubItems[1].Text;
+                txtEmail.Text = item.SubItems[2].Text;
+                txtTelefone.Text = item.SubItems[3].Text;
+
+                //  MessageBox.Show("Id Selecionado = " + id_contato_selecionado);*/
+        }
+
+
+        private void txtBuscar_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtEmail_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+
+                string q = "%" + txtBuscar.Text + "%";
+
+                // Criar a conexão com o MySQL
+                Conexao = new MySqlConnection(data_source);
+
+                string sql = "SELECT * FROM contato WHERE nome LIKE @q OR email LIKE @q";
+
+
+                Conexao.Open();
+
+                // Buscar as informações
+                MySqlCommand buscar = new MySqlCommand(sql, Conexao);
+                buscar.Parameters.AddWithValue("@q", q);
+
+                // armazena as informacoes que temos na busca para mostrar na tela
+                MySqlDataReader reader = buscar.ExecuteReader();
+
+
+                lstContatos.Items.Clear();
+
+                while (reader.Read())
+                {
+                    string[] row =
+                    {
+                        // obtendo as informações do banco de dados (vetor de strings)
+                        reader.GetInt32(0).ToString(), // id
+                        reader.GetString(1),           // nome
+                        reader.GetString(2),           // email
+                        reader.GetString(3),           // telefone
+                    };
+
+                    var linha_list_view = new ListViewItem(row);
+                    lstContatos.Items.Add(linha_list_view);
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                Conexao.Close();
+            }
+        }
+
+        private void lstContatos_ItemSelectionChanged(object sender, ListViewItemSelectionChangedEventArgs e)
+        {
+            ListView.SelectedListViewItemCollection itens_selecionados = lstContatos.SelectedItems;
+
+            // percorrendo a coleção de itens dentro da lista itens_selecionados
+            // Obs¹: A minha linha toda é um item, que contem os subItems (colunas) que desejo selecionar
+
+            foreach (ListViewItem item in itens_selecionados)
+            {
+                id_contato_selecionado = Convert.ToInt32(item.SubItems[0].Text);
+                // extrai o valor de cada uma das variáveis (colunas)
+                txtNome.Text = item.SubItems[1].Text;
+                txtEmail.Text = item.SubItems[2].Text;
+                txtTelefone.Text = item.SubItems[3].Text;
+
+                //  MessageBox.Show("Id Selecionado = " + id_contato_selecionado);
+            }
+        }
+
+    }
+}
